33f94225828a7228396171aede3e35f60ad5e3fa
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-10 01:53:29 +0530

    Update PaintFence.java

--- Pattern3/PaintFence.java
+++ Pattern3/PaintFence.java
@@ -49,3 +49,19 @@ public class Main {
        System.out.println(total);
     }
 }
+
+// gfg same he 
+    long countWays(int n,int k)
+    {
+       if(n == 1) return k;
+       long same = k*1;
+       long diff = k*(k-1);
+       long total = same + diff;
+       
+       for(int i = 3; i <= n; i++){
+           same = diff;
+           diff = total*(k-1);
+           total = (same + diff) % 1000000007;
+       }
+       return total;
+    }
