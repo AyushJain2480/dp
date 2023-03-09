80c6dca9ecdefbe9d5ee0bc079acbe0cf73ab594
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-09 20:51:39 +0530

    Update PaintHouseManyColors.java

--- Pattern3/PaintHouseManyColors.java
+++ Pattern3/PaintHouseManyColors.java
@@ -48,3 +48,66 @@ public class Main {
         System.out.println(min);
     }
 }
+
+
+// O(n2)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int[][] arr = new int[n][c];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        int[][] dp = new int[arr.length][arr[0].length];
+        
+        int least = Integer.MAX_VALUE;
+        int sleast = Integer.MAX_VALUE;
+        
+        for(int j = 0; j < dp[0].length; j++){
+            dp[0][j] = arr[0][j];
+            
+            //  x 7(l) x  12(sL)  x  agar dp[0][j] sL se jyada aya to koi chng ni hoga agar sirf sL se kam hua to sL chng hoga 
+            // or agar L se bhi chota hua to l and sl dono chng honge 
+            if(dp[0][j] <= least){
+                sleast = least;
+                least = dp[0][j];
+            }
+            else if(dp[0][j] <= sleast){
+                sleast = dp[0][j];
+            }
+        }
+        
+        for(int i = 1; i < dp.length; i++){
+            int nleast = Integer.MAX_VALUE;
+            int nsleast = Integer.MAX_VALUE;
+            for(int j = 0; j < dp[0].length; j++){
+               if(least == dp[i - 1][j]){
+                   dp[i][j] = arr[i][j] + sleast;
+               }else{
+                   dp[i][j] = arr[i][j] + least;
+               }
+               
+               if(dp[i][j] <= nleast){
+                nsleast = nleast;
+                nleast = dp[i][j];
+                }
+                else if(dp[i][j] <= nsleast){
+                    nsleast = dp[i][j];
+                }
+            }
+            least = nleast;
+            sleast = nsleast;
+        }
+        System.out.println(least); // akhri row ka least 
+    }
+}
