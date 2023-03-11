bb5f8f4c9ee2d455fce68ed3799bc81e99959416
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-11 15:32:03 +0530

    Update InfiniteTransWithFees.java

--- Pattern4/InfiniteTransWithFees.java
+++ Pattern4/InfiniteTransWithFees.java
@@ -1,4 +1,30 @@
 // Infinite transactions he par sell karne ke time fees bhi lgegi ab isme batana he ki maxm profit kese nikal skte ho 
+
+
+
+// (1) buy status:
+// buy[i] represents the max profit at day i in buy status, given that the last action you took is a buy action at day K, where K<=i. And you have the right to sell at day i+1, or do nothing.
+// (2) sell status:
+// sell[i] represents the max profit at day i in sell status, given that the last action you took is a sell action at day K, where K<=i. And you have the right to buy at day i+1, or do nothing.
+
+// Let's walk through from base case.
+
+// Base case:
+// We can start from buy status, which means we buy stock at day 0.
+// buy[0]=-prices[0];
+// Or we can start from sell status, which means we sell stock at day 0.
+// Given that we don't have any stock at hand in day 0, we set sell status to be 0.
+// sell[0]=0;
+
+// Status transformation:
+// At day i, we may buy stock (from previous sell status) or do nothing (from previous buy status):
+// buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
+// Or
+// At day i, we may sell stock (from previous buy status) or keep holding (from previous sell status):
+// sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+
+// Finally:
+// We will return sell[last_day] as our result, which represents the max profit at the last day, given that you took sell action at any day before the last day.
 import java.io.*;
 import java.util.*;
 
@@ -19,8 +45,8 @@ public class Main {
         for(int i = 1; i < arr.length; i++){
             int nbsp = 0;
             int nssp = 0;
-            nbsp = Math.max(ssp - arr[i], bsp);
-            nssp = Math.max(arr[i] + bsp - charge, ssp);
+            nbsp = Math.max(ssp - arr[i], bsp); // // keep the same as day i-1, or buy from sell status at day i-1
+            nssp = Math.max(arr[i] + bsp - charge, ssp);// // keep the same as day i-1, or sell from buy status at day i-1
             bsp = nbsp;
             ssp = nssp;
         }
