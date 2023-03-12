d983df2835af11f7ec786acb6469dfcdf5619459
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-12 13:31:49 +0530

    Update KTransactions.java

--- Pattern4/KTransactions.java
+++ Pattern4/KTransactions.java
@@ -1,3 +1,5 @@
+// hard to explain whithout matrix check notes
+// O(n3)
 class Solution {
     public int maxProfit(int p, int[] prices) {
         int[][] dp = new int[p + 1][prices.length];
@@ -9,6 +11,21 @@ class Solution {
                     max = Math.max(max , dp[i - 1][k] + (prices[j] - prices[k]));
                 }
                 dp[i][j] = Math.max(dp[i][j - 1], max);
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
+// O(n2) 
+class Solution {
+    public int maxProfit(int p, int[] prices) {
+        int[][] dp = new int[p + 1][prices.length];
+        
+        for(int i = 1; i < dp.length; i++){
+            int max = Integer.MIN_VALUE;
+            for(int j = 1; j < dp[0].length; j++){
+                max = Math.max(dp[i - 1][j - 1] - prices[j - 1] , max);
+                dp[i][j] = Math.max(max + prices[j] , dp[i][j - 1]);
             }
         }
         return dp[dp.length - 1][dp[0].length - 1];
