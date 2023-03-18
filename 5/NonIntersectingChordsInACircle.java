27660820bda70e1be3402ef268405f7376b75f3c
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-18 23:13:14 +0530

    Update NonIntersectingChordsInACircle.java

--- 5/NonIntersectingChordsInACircle.java
+++ 5/NonIntersectingChordsInACircle.java
@@ -1,3 +1,11 @@
+// we are calculating for 2n points thats why it is same as catalan
+// but if we have to calculate for n points than ans will be c(n/2)
+// ye bhi vesa hi he ki ek chord bnalo fir uske clockwise me kitne he * uske anti clockwise me kitne he
+// agar clockwise me 0 to C0 agar anticlockwise me 4 he to C2
+// agar anticlock me 2 he to C0 lenge 
+// jese clockwise me 0 points he uska mtlb he ki 0 points ko kitne tarike se connect krskte he C0
+// jese anticlockwise me 4 points he uska mtlb he ki 4 points ko kitne tarike se connect krskte he C2
+
 import java.io.*;
 import java.util.*;
 
