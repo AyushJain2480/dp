d12cc00582ad43f58f706071b1cffa41cf3af4b4
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-18 10:20:32 +0530

    Create

---
+++ 5/CountingValleysAndMountains.java
@@ -0,0 +1,5 @@
+// 1. You are given a number n, representing the number of upstrokes / and number of downstrokes .
+// 2. You are required to find the number of valleys and mountains you can create using strokes.
+// Note -> At no point should we go below the sea-level. (number of downstrokes should never be more than number of upstrokes).
+
+// This is also a catalan number.
