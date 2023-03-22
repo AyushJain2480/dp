242080d955df2b4e9ceaec235a9574ce772db346
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-22 17:59:56 +0530

    Update MinPalindromicCut.java

--- CutsProblem/MinPalindromicCut.java
+++ CutsProblem/MinPalindromicCut.java
@@ -62,3 +62,53 @@ class Solution {
 
 
 // O(n2)
+
+class Solution {
+    public int minCut(String s) {
+    	boolean[][] dp = new boolean[s.length()][s.length()];
+		
+	for(int g = 0; g < dp.length; g++){
+	    for(int i = 0,j = g; j < dp.length; i++,j++){
+		if(g == 0){
+		    dp[i][j] = true;
+		}
+		else if(g == 1){
+		    if(s.charAt(i) == s.charAt(j)){
+			dp[i][j] = true;
+		    }
+		}
+		else{
+		    if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == true){
+			dp[i][j] = true;
+		    }else{
+			dp[i][j] = false;
+		    }
+		}
+	    }
+	}
+		
+        int[] strg = new int[s.length()];
+        strg[0] = 0;
+        
+        // total string is 0 to j
+        // if i to j is suffix 
+        // than 0 to i - 1 is prefix
+        for(int j = 1; j < strg.length; j++){
+            if(dp[0][j]){
+                strg[j] = 0;
+            }else{
+                int min = Integer.MAX_VALUE;
+                for(int i = j; i >= 1; i--){
+                    if(dp[i][j]){
+                        // 0 to i - 1 is prefix
+                        if(strg[i - 1] < min){
+                            min = strg[i - 1];
+                        }
+                    }
+                }
+                strg[j] = min + 1;
+            }
+        }
+        return strg[strg.length - 1];
+    }
+}
