5d190b2105750a673635462bc12617ea5fe932ba
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-03-27 21:00:16 +0530

    Update MinmAsciiDeleteSumForTwoStrings.java

--- StringDP/MinmAsciiDeleteSumForTwoStrings.java
+++ StringDP/MinmAsciiDeleteSumForTwoStrings.java
@@ -27,3 +27,31 @@ class Solution {
         return dp[0][0];
     }
 }
+// i thaught it like we have to delete all those character which are not a part of longest common subsequence . Hence getting minimum number of possible deletes and hence minimum ASCII sum.
+class Solution {
+   public int minimumDeleteSum(String s1, String s2) {
+    int lcs = getLcs(s1, s2); // get the LCS using the modified method
+    int sum = 0;
+    for (char c : s1.toCharArray()) {
+        sum += (int) c;
+    }
+    for (char c : s2.toCharArray()) {
+        sum += (int) c;
+    }
+    return sum - 2 * lcs; // calculate the minimum ASCII delete sum
+}
+
+public int getLcs(String s1, String s2) {
+   int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for(int i = dp.length - 2; i >= 0; i--){
+            for(int j = dp[0].length - 2; j >= 0; j--){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = dp[i + 1][j + 1] + (int) s1.charAt(i);
+                }else{
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+}
+}
